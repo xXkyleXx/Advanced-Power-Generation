ea6775f4902e33693b8ff0ec48c64539d3b64746
neeyk <kyle_aughinbaugh@yahoo.com>
2014-07-12 22:00:43 -0500

    .

--- src/main/java/com/xXkyleXx/apg/blocks/Blockapg.java
+++ src/main/java/com/xXkyleXx/apg/blocks/Blockapg.java
@@ -3,10 +3,14 @@ package com.xXkyleXx.apg.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
-public class Blockapg extends Block{
-	
-	public Blockapg(Material material){
+public class Blockapg extends Block {
+
+	public Blockapg(Material material) {
 		super(material);
+	}
+
+	public Blockapg() {
+		this(Material.rock);
 	}
 
 }
